9cf82f58a6a17b9f5e000fccd22dc5d531b1bed6
radne0 <84048659+radne0@users.noreply.github.com>
2021-05-30 17:52:58 -0400

    Add files via upload

--- bb.py
+++ bb.py
@@ -6,8 +6,6 @@ import pyperclip as cb
 import datetime as dt
 from boltons.setutils import IndexedSet
 
-
-
 ''' A basic buff bot.
 logfile - path to eq log file to monitor for buff requests. eg:  /Everquest/logs/eqlog_Fluffy_mangler.txt
 buffs - dictionary  in the form:  "bufftriggertext":(gem_slot,casttime)
@@ -21,8 +19,8 @@ class BuffBot:
        self.logfile = logfile
        self.bufflist = buffs
        self.rx = re.compile(regex)
-       self.buffq = IndexedSet()      # Actual QUEUE.
        self.windowName = "EverQuest"
+       self.buffqueue = IndexedSet()
 
     #display configuration   
     def displayconfig(self):
@@ -31,12 +29,8 @@ class BuffBot:
         print("Current Buff list")
         for key in self.bufflist:
             print("buff trigger:",key,"\tgem slot: ",self.bufflist[key][0],'\tcast time:',self.bufflist[key][1])
-            
-    #def checkWindow(self):
-    #    hwnd = w32.GetForegroundWindow()
-    #    name = w32.GetWindowText(hwnd)
-    #    return self.windowName == name
-     
+
+    # Log matches Process
     def MatchFound(self,name,request):
 
            try:
@@ -51,11 +45,10 @@ class BuffBot:
     def ProcessRequest(self,name,request,spellinfo):
         gem = spellinfo[0]
         casttime = spellinfo[1]
-        castline =["/tar " + name, "/cast " + str(gem), "/cast " + str(gem),"/cast " + str(gem),"/cast " + str(gem)]
+        castline =["/tar " + name, "/cast " + str(gem), "/cast " + str(gem),"/cast " + str(gem)]
         print(str(dt.datetime.now())[0:19],'casting',request,'on',name,'from spell slot',gem, 'with cast time ',casttime,'seconds.')            
         
         for lines in castline:
-            print(lines)
             self.enterline(lines)               
             time.sleep(0.5)
         time.sleep(float(casttime))
@@ -71,30 +64,38 @@ class BuffBot:
         kb.keyUp("ctrl", _pause=False)
         kb.press("enter", _pause=False)
 
-
-   # wait for a valid buff request from a player.          
+   # main thread, wait for a valid buff request then add it to the queue.
     def listen(self):
        with open(self.logfile) as f:
              f.seek(0, os.SEEK_END)
+             print(str(dt.datetime.now())[0:19],"Listening for buff requests")
              time.sleep(2)
-             print("Listening for buff requests")
              while True:
                  txt=''
                  line = f.readline()
 
                  if line:
-                     #print("raw line: " + line)
                      s = line.split()       # get rid of time stamps
                      txt = " ".join(s[5:])
                      txt = txt.lower()
                      m = self.rx.match(txt)
                      if m:
-                         self.MatchFound(m.group(1),m.group(2))
-                 time.sleep(0.3)
+                         self.addQueue(m.group(1),m.group(2))
+                 time.sleep(0.1)
 
+    # If queue isn't empty get to work.
+    def monitorQueue(self):
+        print(str(dt.datetime.now())[0:19], "Monitoring Queue")
+        while(True):
+            while (len(self.buffqueue)):
+                print(str(dt.datetime.now())[0:19], "Processing Queue: ", str(len(self.buffqueue)) + " items left.")
+                rq = self.buffqueue.pop(0)
+                self.MatchFound(rq[0],rq[1])
+            time.sleep(0.25)
 
-
-
-
-
-  
+    # Add an request to queue.
+    def addQueue(self, name, req):
+        print(str(dt.datetime.now())[0:19], "Adding " + name + " to queue for: " + req)
+        request = (name,req)
+        self.buffqueue.add(request)
+        print(self.buffqueue)
--- main.py
+++ main.py
@@ -2,24 +2,28 @@
 import time
 from bb import BuffBot
 import configparser as cfp
-
+import threading
 
 if __name__ == "__main__":
 
-    reg_ex="^(\w+) tells you, \'(\w+)\'"
+    reg_ex = "^(\w+) tells you, \'(\w+)\'"
     config = cfp.ConfigParser()
     config.read('config.ini')
     logpath = config['PATHINFO']['PATH_TO_LOG'] + config['PATHINFO']['LOG_NAME']
-    buffs ={}
+    buffs = {}
     print("v2")
 
     # create dictionary with buff trigger as the key and (gemslot,casttime) as the value
     for buff in config['BUFFINFO']:
         print(buff)
         bl = config['BUFFINFO'][buff].split(',')
-        buffs[buff] = (bl[0],bl[1]) 
-    s = BuffBot(logpath,buffs,reg_ex)
+        buffs[buff] = (bl[0], bl[1])
+    s = BuffBot(logpath, buffs, reg_ex)
     s.displayconfig()
-    time.sleep(1)
-    s.listen()
-
+    print()
+    time.sleep(3)
+    readlog = threading.Thread(target=s.listen)
+    readlog.start()
+    time.sleep(0.1)
+    monitorqueue = threading.Thread(target=s.monitorQueue)
+    monitorqueue.start()
